19a5d05fa7906e129f1f7aea7daad62169cb7b08
Kateryna Kraieva <ketryin@ukr.net>
2021-01-11 00:07:30 +0200

    adds tasks 1-3
    adds tasks 1-3

---
+++ js/task-3.js
@@ -0,0 +1,36 @@
+class Storage{
+    constructor(items) {
+        this.items = items;
+    }
+
+    getItems() {
+        return this.items;
+    }
+
+    addItem(item) {
+        this.items.push(item); 
+    }
+
+    removeItem(item) {
+        this.items = this.items.filter(function (element) {
+            return element !== item;
+        });
+    }
+}
+
+
+const storage = new Storage([
+  'Нанитоиды',
+  'Пролонгер',
+  'Железные жупи',
+  'Антигравитатор',
+]);
+
+const items = storage.getItems();
+console.table(items); // [ "Нанитоиды", "Пролонгер", "Железные жупи", "Антигравитатор" ]
+
+storage.addItem('Дроид');
+console.table(storage.items); // [ "Нанитоиды", "Пролонгер", "Железные жупи", "Антигравитатор", "Дроид" ]
+
+storage.removeItem('Пролонгер');
+console.table(storage.items); // [ "Нанитоиды", "Железные жупи", "Антигравитатор", "Дроид" ]
